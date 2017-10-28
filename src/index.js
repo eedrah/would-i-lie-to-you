4cb297e1358bb38dd07481080db176ac81630368
Eedrah <eedrah@users.noreply.github.com>
2017-10-28 05:30:07 -0500

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, ApplyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducer from './reducer'
+
+import './index.css'
+
+const middleware = [thunk]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+const store = createStore(reducer, ApplyMiddleware(...middleware))
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
