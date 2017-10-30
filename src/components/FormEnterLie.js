db835b12384e8666ff2973d46b4b7e65e3de5106
Eedrah <eedrah@users.noreply.github.com>
2017-10-30 18:23:04 -0500

    WIP

--- src/components/FormEnterLie.js
+++ src/components/FormEnterLie.js
@@ -2,14 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './FormEnterLie.css'
 
-function FormEnterLie({ onSubmit }) {
+function FormEnterLie({
+  onSubmitLie,
+  isPostingLie,
+  proposedLiesimethingsmething,
+}) {
   return (
     <div className={styles.base}>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          onSubmitLie(e.target.value.get('enteredLie'))
+        }}
+      >
         <label>
           Enter lie:
-          <input type="text" />
+          <input type="text" name="enteredLie" />
         </label>
+        <input type="submit" disabled={isPostingLie} />
       </form>
     </div>
   )
@@ -18,7 +28,7 @@ function FormEnterLie({ onSubmit }) {
 FormEnterLie.defaultProps = {}
 
 FormEnterLie.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onSubmitLie: PropTypes.func.isRequired,
   isPostingLie: PropTypes.bool.isRequired,
   proposedLie: PropTypes.string.isRequired,
   errorHasOccurred: PropTypes.bool.isRequired,
