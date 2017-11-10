c1e8f3a43c74b95464c139a91b7d8922a67e2c9c
Eedrah <eedrah@users.noreply.github.com>
2017-11-09 23:47:02 -0500

    add normalize

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import reducer from './reducer'
 
+import 'normalize.css'
 import './index.css'
 
 if (process.env.NODE_ENV !== 'production') {
